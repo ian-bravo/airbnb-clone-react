01c784350b12a374c33e0767d42a9e545374e7b9
ian-bravo <bravo.ian@gmail.com>
2023-12-27 18:18:07 -0800

    add AddPlaceForm
    Co-authored-by: Jon Cheng <joncheng.dev@gmail.com>

--- src/component/AddPlaceForm.js
+++ src/component/AddPlaceForm.js
@@ -1,9 +1,40 @@
 import React from "react";
 
-function AddPlaceForm() {
+function AddPlaceForm(props) {
+  function whenSubmitClicked(event) {
+    event.preventDefault();
+
+    props.handleAddingNewPlaceToList({
+      location: event.target.location.value,
+      date: event.target.date.value,
+      description: event.target.description.value,
+      price: event.target.price.value,
+    });
+
+    console.log("Location input:", event.target.location.value);
+    console.log("Date input:", event.target.date.value);
+    console.log("Description input:", event.target.description.value);
+    console.log("Price input:", event.target.price.value);
+  }
+  
   return (
     <>
-      <h2>Add Place Form</h2>  
+    <h2>Add Place Form</h2>  
+      <form onSubmit={whenSubmitClicked}>
+        <label for="location">Add a location:</label>
+        <input type="text" name="location" />
+        <br/>
+        <label for="date">Add a date:</label>
+        <input type="text" name="date" />
+        <br/>
+        <label for="description">Add a description:</label>
+        <input type="text" name="description"/>
+        <br/>
+        <label for="price">Add a price:</label>
+        <input type="text" name="price"/>
+        <br/>
+        <button type="submit">Add a place</button>
+    </form>
     </>
   );
 }
--- src/component/Control.js
+++ src/component/Control.js
@@ -1,29 +1,77 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import PlaceList from "./PlaceList";
 import AddPlaceForm from "./AddPlaceForm";
 
 function Control() {
   const [formVisibleOnPage, setFormVisibleOnPage] = useState(false);
+  const [usePlaceList, setUsePlaceList] = useState([]);
+
+  // useEffect(() => {
+  //   console.log("handleAddingNewPlaceToList Result: ", usePlaceList);
+  // }, []);
 
   const handleClick = () => {
     setFormVisibleOnPage(!formVisibleOnPage);
   }
-
+  
+  const handleAddingNewPlaceToList = (newPlace) => {
+    const newPlaceList = [...usePlaceList, newPlace];
+    setUsePlaceList(newPlaceList);
+    // usePlaceList --> needs to be copied
+    // new entry coming from form --> this will be added to "usePlaceList"
+    // setUsePlaceList(**everything in here is now THE LIST**)
+  }
+//need to display the list - 27 Dec 2023
 
   let currentlyVisibleComponent = null;
+  let buttonText = null;
+
   if (formVisibleOnPage) {
-    currentlyVisibleComponent = <AddPlaceForm />;
+    currentlyVisibleComponent = <AddPlaceForm handleAddingNewPlaceToList={ handleAddingNewPlaceToList} />;
+    buttonText = "Back to Place List";
   } else {
     currentlyVisibleComponent = <PlaceList />;
+    buttonText = "Add New Place";
   }
 
   return (
     <>
       <h2>Control</h2>
       {currentlyVisibleComponent}
-      <button onClick={handleClick}>Toggle</button>
+      <button onClick={handleClick}>{buttonText}</button>
     </>
   );
 }
 
 export default Control;
+
+
+
+
+  // const testList = [
+  //   {
+  //     location: "Seattle, Washington",
+  //     description: "cozy, close to attractions",
+  //     date: "Jan 4th",
+  //     price: "100",
+  //   },
+  //   {
+  //     location: "Seattle, Washington",
+  //     description: "cozy, close to attractions",
+  //     date: "Jan 4th",
+  //     price: "100",
+  //   },
+  //   {
+  //     location: "Seattle, Washington",
+  //     description: "cozy, close to attractions",
+  //     date: "Jan 4th",
+  //     price: "100",
+  //   }
+  // ];
+
+  // const addEntryToTestList = {
+  //   location: "Seattle, Washington",
+  //   description: "cozy, close to attractions",
+  //   date: "Jan 4th",
+  //   price: "100",
+  // };
