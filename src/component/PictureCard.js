0bf11c41a9543fb1aef5a6c1b5b381a81ed98ae2
ian-bravo <bravo.ian@gmail.com>
2023-12-28 15:19:21 -0800

    add card UI
    Co-authored-by: Jon Cheng <joncheng.dev@gmail.com>

---
+++ src/component/PictureCard.js
@@ -0,0 +1,95 @@
+import React from "react";
+
+function PictureCard(props) {
+  const {children} = props;
+  
+  const divStyle = {
+    width: '200px',
+    height: '200px',
+    border: '1.5px solid black',
+    borderRadius: '2px',
+    padding: '4px',
+    margin: '4px',
+    backgroundColor: "#FBF4E2",
+  }
+  
+  return (
+    <div style={divStyle}>
+      {children}
+    </div>
+  );
+}
+
+export default PictureCard;
+
+
+// EPICODUS WAY:
+// function PictureCard(props) {   <----------props
+  
+//   const divStyle = {
+//     width: '200px',
+//     height: '200px',
+//     border: '1.5px solid black',
+//     borderRadius: '2px',
+//     padding: '4px',
+//     margin: '4px',
+//     backgroundColor: "#FBF4E2",
+//   }
+  
+//   return(
+//       <div style={divStyle}>
+//         {props.children}  <------------------
+//       </div>
+
+
+//   )
+// }
+
+// export default PictureCard;
+
+
+// DESTRUCTURING PROPS
+// function PictureCard(props) {
+//   const {children} = props;
+  
+//   const divStyle = {
+//     width: '200px',
+//     height: '200px',
+//     border: '1.5px solid black',
+//     borderRadius: '2px',
+//     padding: '4px',
+//     margin: '4px',
+//     backgroundColor: "#FBF4E2",
+//   }
+  
+//   return (
+//     <div style={divStyle}>
+//       {children}
+//     </div>
+//   );
+// }
+
+// export default PictureCard;
+
+
+// SHORTCUT
+// function PictureCard({children}) {
+  
+//   const divStyle = {
+//     width: '200px',
+//     height: '200px',
+//     border: '1.5px solid black',
+//     borderRadius: '2px',
+//     padding: '4px',
+//     margin: '4px',
+//     backgroundColor: "#FBF4E2",
+//   }
+  
+//   return (
+//     <div style={divStyle}>
+//       {children}
+//     </div>
+//   );
+// }
+
+// export default PictureCard;
--- src/component/Place.js
+++ src/component/Place.js
@@ -1,14 +1,14 @@
 import React from "react";
+import PictureCard from "./PictureCard";
 
 function Place(props) {
   return (
-    <>
+    <PictureCard>
       <h3>{props.location}</h3>
       <p>{props.description}</p>
       <p>{props.date}</p>
       <p>{props.price}</p>
-      <hr/>
-    </>
+    </PictureCard>
   );
 }
 
